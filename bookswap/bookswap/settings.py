1f49c320623e37cfd57e83281cad662c986076c7
SatvickShekhawat31 <148701762+SatvickShekhawat31@users.noreply.github.com>
2024-06-24 16:29:21 +0530

    Update settings.py

--- bookswap/bookswap/settings.py
+++ bookswap/bookswap/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!va^hk-(+tlhf7j-g(4y2rmdrfpp)sfg3w(vqrkwl90gwc*9e^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
